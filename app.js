c39f99dd351ae05b6c595747ead78c91b0d88146
Mariana Apolaya <apolayaschradermariana@gmail.com>
2025-04-19 16:55:00 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path'); 
 const conexion = require('./database/db');
+const router = require('./router');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));  // Indica la ubicación de las vistas
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json()); 
+app.use(express.json());
 
-app.use('/', require('./router')); 
+app.use('/', router);  // Usa las rutas definidas en router.js
 
 app.listen(5000, () => {
   console.log('Servidor ejecutándose en http://localhost:5000');
--- router.js
+++ router.js
@@ -2,25 +2,24 @@ const express = require('express');
 const router = express.Router();
 const conexion = require('./database/db');
 
-// Ruta para mostrar todos los empleados
+// Ruta para mostrar los empleados
 router.get('/', (req, res) => {
-  // Consulta SQL para obtener todos los registros de la tabla empleados
   conexion.query("SELECT * FROM empleados", (error, results) => {
     if (error) {
-      throw error;
-    } else {
-      // Renderiza la vista 'index' y pasa los resultados a la vista
-      res.render('index', { registros: results });
+      console.error(error);
+      return res.status(500).send("Error en la base de datos");
     }
+    // Renderiza la vista 'index' y pasa los datos (empleados)
+    res.render('index', { registros: results });
   });
 });
 
-// Ruta para mostrar el formulario de registro de un nuevo empleado
+// Ruta para crear nuevos empleados
 router.get('/create', (req, res) => {
   res.render('create');
 });
 
-// Ruta para guardar un nuevo empleado
+// Ruta para guardar nuevos empleados
 const crud = require('./controllers/crud');
 router.post('/save', crud.save);
 
